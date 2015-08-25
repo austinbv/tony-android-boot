e91b6d24ed1d27fddb790dc7a829c2447983702f
Anthony Tarantini <anthony.tarantini@gmail.com>
2015-08-25 17:11:37 -0500

    removing unneeded provides in test module

--- src/test/java/io/pivotal/labsboot/alkyhol/TestAlkyholModule.java
+++ src/test/java/io/pivotal/labsboot/alkyhol/TestAlkyholModule.java
@@ -1,13 +1,13 @@
 package io.pivotal.labsboot.alkyhol;
 
-import java.util.concurrent.ExecutorService;
-
 import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
 
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
 
 @Module(
         injects = {
@@ -25,18 +25,6 @@ import static org.mockito.Mockito.mock;
 public class TestAlkyholModule {
     @Provides
     @Singleton
-    AlkyholApiClient providesApiClient() {
-        return mock(AlkyholApiClient.class);
-    }
-
-    @Provides
-    @Singleton
-    ExecutorService providesExecutor() {
-        return mock(ExecutorService.class);
-    }
-
-    @Provides
-    @Singleton
     AlkyholListDelegate providesDelegate() {
         return mock(AlkyholListDelegate.class);
     }
@@ -50,6 +38,8 @@ public class TestAlkyholModule {
     @Provides
     @Singleton
     AlkyholListAdapter providesAdapter() {
-        return new TestAdapter();
+        final TestAdapter testAdapter = spy(new TestAdapter());
+        doNothing().when(testAdapter).notifyDataSetChanged();
+        return testAdapter;
     }
 }
